26450e9926a042a3ab7bdc2151f13b81f3d83b6b
Chitra Rajavel <chitrarajavel@gmail.com>
2023-12-09 15:41:27 -0600

    fix: creating database structure

--- src/modules/Api.js
+++ src/modules/Api.js
@@ -1,11 +1,46 @@
 class Api {
     constructor() {
+        // localstorage reference key
+        this.databaseKey = 'GoT_DB';
         this.localstorage = localStorage;
+        // [{firstName:"", lastName:"", email:"", password: "", favorites: {books:[], characters: [], houses:[]}}]
+        this.database = {users: []}; // store firstName, lastName, email, password, and favorites object
     }
 
-    userExists(email) {
-        return this.localstorage.getItem(email) ? true : false;
+    /**
+     * Save database object {key = databaseKey, value = database }
+     */
+
+    saveDatabase() {
+        this.localstorage.setItem(
+            this.databaseKey,
+            JSON.stringify(this.database)
+        );
     }
+
+    /**
+     * Load database object and set it to this.database
+     */
+
+    loadDatabase() {
+        let db = this.localstorage.getItem(this.databaseKey);
+        if (db) {
+            this.database = JSON.parse(db);
+        }
+    }
+
+    /**
+     *  returns a boolean to validate if the email exists on the signup form
+     */
+
+    userExists(email) {
+        let dbUsers = this.database.users;
+        return dbUsers.filter(user => (email === user.email ? true : false));
+    }
+
+    /**
+     *
+     */
 
     registerUser(user) {
         //already validating in formik
@@ -16,6 +51,11 @@ class Api {
         this.localstorage.setItem(user.email, strUser);
         return {success: true};
     }
+
+    /**
+     *
+     */
+
     getUser(email, password) {
         if (!email && !password) {
             return null;
@@ -25,6 +65,11 @@ class Api {
         let userObj = strUser ? JSON.parse(strUser) : null;
         return userObj && password === userObj.password ? userObj : null;
     }
+
+    /**
+     *
+     */
+
     removeUser(user) {
         if (!user.email) {
             return;
